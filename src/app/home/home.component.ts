b32b8b8f9cd79ceffdfa5a31f25ad14afb4551e8
akhilesh <akhilesh.kamble0807@gmail.com>
2020-05-18 14:37:25 +0530

    new start

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-=======
 import { MatDialog, MatDialogRef } from '@angular/material';
 import {  RegisterformComponent } from '../registerform/registerform.component';
->>>>>>> fc7f55190b0e9dc1cc0436293073b68d8ae9a8d7
 
 @Component({
   selector: 'app-home',
@@ -12,12 +9,6 @@ import {  RegisterformComponent } from '../registerform/registerform.component';
 })
 export class HomeComponent implements OnInit {
 
-<<<<<<< HEAD
-  constructor() { }
-
-  ngOnInit() {
-  }
-=======
   constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
@@ -25,6 +16,5 @@ export class HomeComponent implements OnInit {
   openRegistrationForm() {
     this.dialog.open(RegisterformComponent, { width: '800px', height: '700px'});
   }
->>>>>>> fc7f55190b0e9dc1cc0436293073b68d8ae9a8d7
 
 }
